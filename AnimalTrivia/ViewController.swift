52433b2c2de9d657c1e837008316dfa1a5e94bef
YarinDev <yarin.software@gmail.com>
2022-05-27 11:06:20 +0200

    Initial Commit

---
+++ AnimalTrivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimalTrivia
+//
+//  Created by user216781 on 5/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
