bb1fed0a9ba92e72254730f0e81864780c3812e7
YarinDev <yarin.software@gmail.com>
2022-05-27 12:27:21 +0200

    v.1

--- AnimalTrivia/ViewController.swift
+++ AnimalTrivia/ViewController.swift
@@ -9,10 +9,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    @IBOutlet weak var lastScore_LBL: UILabel!
+
+        
+
+        override func viewDidLoad() {
+
+            super.viewDidLoad()
+
+            lastScore_LBL.isHidden = true
+
+            NotificationCenter.default.addObserver(self, selector: #selector(getScore(_:)), name: Notification.Name("score"), object: nil)
+
+        }
+
+        
+
+        @objc func getScore(_ notification: Notification){
+
+            let score = notification.object as! String?
+
+            lastScore_LBL.isHidden = false
+
+            lastScore_LBL.text = "Latest game score: " + score!
+
+        }
+
+
+
+        @IBAction func startGame(_ sender: Any) {
+
+            guard let vc = storyboard?.instantiateViewController(withIdentifier: "GameScreen") as? GameViewController else{
+
+                return
+
+            }
+
+            vc.modalPresentationStyle = .fullScreen
+
+            vc.modalTransitionStyle = .coverVertical
+
+            present(vc, animated: true)
+
+        }
 
 
 }
